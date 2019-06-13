1f0fc1f489e8404f9ffa6d08f32eb6a28d1cdbb1
ReoKuriyama <kuriyama.reo@moneyforward.co.jp>
2019-06-13 13:50:34 +0900

    a

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_040730) do
+ActiveRecord::Schema.define(version: 2019_06_13_042312) do
+
+  create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "group_name"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["reset_password_token"], name: "index_answers_on_reset_password_token", unique: true
+  end
 
   create_table "odds", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "group1"
